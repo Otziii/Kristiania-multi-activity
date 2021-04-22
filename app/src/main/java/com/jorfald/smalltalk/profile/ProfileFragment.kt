4edf8a027dc0802a289996b4452c87153e0c366d
Øivind Jorfald <oivind.jorfald@outlook.com>
2021-04-22 11:46:50 +0200

    Create local notification

--- app/src/main/java/com/jorfald/smalltalk/profile/ProfileFragment.kt
+++ app/src/main/java/com/jorfald/smalltalk/profile/ProfileFragment.kt
@@ -1,5 +1,10 @@
 package com.jorfald.smalltalk.profile
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +26,13 @@ class ProfileFragment : Fragment() {
     private lateinit var firstNameTextView: TextView
     private lateinit var logOutButton: Button
 
+    private lateinit var notificationButton: Button
+
+    private lateinit var notificationManager: NotificationManager
+
+    private val channelId = "com.jorfald.smalltalk.important.notifications"
+    private var counter = 111
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,6 +44,8 @@ class ProfileFragment : Fragment() {
         firstNameTextView = view.findViewById(R.id.first_name_text_view)
         logOutButton = view.findViewById(R.id.log_out_button)
 
+        notificationButton = view.findViewById(R.id.notification_button)
+
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         return view
@@ -39,6 +53,10 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        notificationManager =
+            requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel()
 
         val user = UserManager.loggedInUser
 
@@ -49,5 +67,38 @@ class ProfileFragment : Fragment() {
         logOutButton.setOnClickListener {
             (activity as? MainActivity)?.logOutUser()
         }
+
+        notificationButton.setOnClickListener {
+            createAndSendNotification()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            channelId,
+            "Important messages",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        channel.description = "Only highly important messages"
+        channel.enableLights(true)
+        channel.lightColor = Color.RED
+        channel.enableVibration(true)
+        channel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createAndSendNotification() {
+        val notificationId = counter
+
+        val notification = Notification.Builder(activity, channelId)
+            .setContentTitle("Very important news!")
+            .setContentText("This is extremely important: Hello World!")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .build()
+
+        notificationManager.notify(notificationId, notification)
+        counter++
     }
 }
