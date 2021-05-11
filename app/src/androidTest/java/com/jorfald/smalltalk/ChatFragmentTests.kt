c67ae67755ea53ed186b212be4a670c5bd50cc07
Konstantin Loginov <loginov.k@gmail.com>
2021-05-11 23:15:42 +0200

    Getting test to green-state

--- app/src/androidTest/java/com/jorfald/smalltalk/ChatFragmentTests.kt
+++ app/src/androidTest/java/com/jorfald/smalltalk/ChatFragmentTests.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.jorfald.smalltalk.chat.ChatFragment
 import com.jorfald.smalltalk.chat.ChatViewModel
 import com.jorfald.smalltalk.database.UserObject
